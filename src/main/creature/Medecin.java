b703b6af4eb6a67b99ff5e154bc72f33b296e842
MINIERE-Mathias-2326080aa <mathias.miniere@etu.univ-amu.fr>
2025-01-02 18:47:29 +0100

    Changement des attributs de la classe Medecin, ajout d'un nouveau constructeur et toString.

--- src/main/creature/Medecin.java
+++ src/main/creature/Medecin.java
@@ -3,14 +3,33 @@ package main.creature;
 import main.serviceMedical.ServiceMedical;
 
 public class Medecin {
-	private Creature creature;
+	private String type;
+	private String nom;
+	private String sexe;
+	private int age;
 
 	public Medecin(Creature creature) {
 		super();
-		this.creature = creature;
+		this.type = creature.getClass().getSimpleName();
+		this.nom = creature.getNom();
+		this.sexe = creature.getSexe();
+		this.age = creature.getAge();
 	}
 	
+	public Medecin(String type, String nom, String sexe, int age) {
+		super();
+		this.type = type;
+		this.nom = nom;
+		this.sexe = sexe;
+		this.age = age;
+	}
+
 	public void examinerService(ServiceMedical service) {
 		
+	}
+
+	@Override
+	public String toString() {
+		return "Medecin [type=" + type + ", nom=" + nom + ", sexe=" + sexe + ", age=" + age + "]";
 	}
 }
