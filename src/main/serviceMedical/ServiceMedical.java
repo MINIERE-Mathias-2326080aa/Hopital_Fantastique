9def9fc89d57e66f0fd538fea9716a4bb9e936da
MINIERE-Mathias-2326080aa <mathias.miniere@etu.univ-amu.fr>
2025-01-02 18:44:56 +0100

    ServiceMedical fonctions afficherCaracteristiques, ajouterCreature, enleverCreature

--- src/main/serviceMedical/ServiceMedical.java
+++ src/main/serviceMedical/ServiceMedical.java
@@ -20,18 +20,52 @@ public class ServiceMedical {
 	}
 	
 	public void afficherCaracteristiques() {
-		
+		System.out.println("Nom : " + nom);
+		System.out.println("Superficie : " + superficie + "m2");
+		System.out.print("Budget : ");
+		switch(this.budget) {
+			case(0):
+				System.out.println("inexistant");
+				break;
+			case(1):
+				System.out.println("médiocre");
+				break;
+			case(2):
+				System.out.println("insuffisant");
+				break;
+			case(3):
+				System.out.println("faible");
+		}
+		System.out.println("Nombre maximum de créature : " + nbMaxCreatures);
+		System.out.println("Créatures :");
+		for (Creature creature : creatures) {
+			System.out.println(creature);
+		}
 	}
 	
 	public void ajouterCreature(Creature creature) {
-		
+		if (creatures.isEmpty()) {
+			creatures.add(creature);
+		} else {
+			if (creatures.size() >= nbMaxCreatures) {
+				System.out.println("Nombre maximum de creatures atteint.");
+			} else if (creatures.getFirst().getClass() != creature.getClass()) {
+				System.out.println("Les services médicaux peuvent accueillir uniquement des créatures de même type.");
+			} else {
+				creatures.add(creature);
+			}
+		}
 	}
 	
 	public void enleverCreature(Creature creature) {
-		
+		this.creatures.remove(creature);
 	}
 	
 	public void reviserBudget() {
 		
+	}
+
+	public List<Creature> getCreatures() {
+		return creatures;
 	}
 }
