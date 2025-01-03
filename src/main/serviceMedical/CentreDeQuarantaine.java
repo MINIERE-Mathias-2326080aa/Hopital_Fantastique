31a6d9c556fbe10e8fc5c5e8b7b6039ebcdb66ff
MINIERE-Mathias-2326080aa <mathias.miniere@etu.univ-amu.fr>
2025-01-03 11:21:55 +0100

    fonction ajouterCreature.

--- src/main/serviceMedical/CentreDeQuarantaine.java
+++ src/main/serviceMedical/CentreDeQuarantaine.java
@@ -1,9 +1,20 @@
 package main.serviceMedical;
 
+import java.util.Arrays;
+
+import main.creature.*;
+
 public class CentreDeQuarantaine extends ServiceMedical {
 
 	public CentreDeQuarantaine(String nom, int superficie, int nbMaxCreatures, int budget) {
 		super(nom, superficie, nbMaxCreatures, budget);
+	}
+
+	@Override
+	public void ajouterCreature(Creature creature) {
+		if (Arrays.asList(creature.getClass().getInterfaces()).contains(Contaminant.class)) {
+			super.ajouterCreature(creature);
+		}
 	}
 
 	@Override
--- src/main/serviceMedical/Crypte.java
+++ src/main/serviceMedical/Crypte.java
@@ -1,11 +1,23 @@
 package main.serviceMedical;
 
+import java.util.Arrays;
+
+import main.creature.Creature;
+import main.creature.Regenerant;
+
 public class Crypte extends ServiceMedical {
 
 	public Crypte(String nom, int superficie, int nbMaxCreatures, int budget) {
 		super(nom, superficie, nbMaxCreatures, budget);
 	}
 	
+	@Override
+	public void ajouterCreature(Creature creature) {
+		if (Arrays.asList(creature.getClass().getInterfaces()).contains(Regenerant.class)) {
+			super.ajouterCreature(creature);
+		}
+	}
+
 	public void reviserBudget() {
 		
 	}
