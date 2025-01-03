fcd04711a824571535277eadde2021e5f4c37dca
MINIERE-Mathias-2326080aa <mathias.miniere@etu.univ-amu.fr>
2025-01-03 11:20:36 +0100

    Implémentation de Contaminant et fonction semporter.

--- src/main/creature/HommeBete.java
+++ src/main/creature/HommeBete.java
@@ -1,13 +1,35 @@
 package main.creature;
 
+import java.util.List;
+import java.util.Random;
+
 import main.maladie.Maladie;
 
-public class HommeBete extends Creature {
+public class HommeBete extends Creature implements Contaminant{
 
 	public HommeBete(String nom, String sexe, int poids, int taille, int age, int moral, Maladie maladie) {
 		super(nom, sexe, poids, taille, age, moral, maladie);
 	}
 
+	@Override
+	public void semporter(List<Creature> creaturesProches) {
+		super.semporter(creaturesProches);
+		for (Creature creature : creaturesProches) {
+			Random r = new Random();
+			if (r.nextInt(100) > 50) {
+				contaminer(creature);
+			}
+		}
+	}
+	
+	@Override
+	public void contaminer(Creature creature) {
+		Random random = new Random();
+		Maladie maladie = this.getMaladies().get(random.nextInt(this.getMaladies().size()));
+		creature.ajouterMaladie(maladie);
+		System.out.println("La créature " + creature.getNom() + " a été infecté par la maladie " + maladie.getNomComplet());
+	}
+	
 	@Override
 	public void trepasser() {
 		
--- src/main/creature/Vampire.java
+++ src/main/creature/Vampire.java
@@ -1,11 +1,33 @@
 package main.creature;
 
+import java.util.List;
+import java.util.Random;
+
 import main.maladie.Maladie;
 
-public class Vampire extends Creature implements Demoralisant, Regenerant {
+public class Vampire extends Creature implements Demoralisant, Regenerant, Contaminant {
 
 	public Vampire(String nom, String sexe, int poids, int taille, int age, int moral, Maladie maladie) {
 		super(nom, sexe, poids, taille, age, moral, maladie);
+	}
+	
+	@Override
+	public void semporter(List<Creature> creaturesProches) {
+		super.semporter(creaturesProches);
+		for (Creature creature : creaturesProches) {
+			Random r = new Random();
+			if (r.nextInt(100) > 50) {
+				contaminer(creature);
+			}
+		}
+	}
+	
+	@Override
+	public void contaminer(Creature creature) {
+		Random random = new Random();
+		Maladie maladie = this.getMaladies().get(random.nextInt(this.getMaladies().size()));
+		creature.ajouterMaladie(maladie);
+		System.out.println("La créature " + creature.getNom() + " a été infecté par la maladie " + maladie.getNomComplet());
 	}
 
 	@Override
